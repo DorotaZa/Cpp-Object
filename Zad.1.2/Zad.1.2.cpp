3c4fc36d293a1345704106f8a5cb852b724106a6
Dorota Zak <dorotaa.zak@gmail.com>
2022-06-02 09:57:28 +0000

    update- verification on the files

--- Zad.1.2/Zad.1.2.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Zad.1.2
-//.3. Zamodeluj klasę kalkulatora (Calculator), który opisuje funkcjonalność prostego kalkulatora z 4 operacjami
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
